1ee62afe9689b91856767a5016c7f09d34821eb8
Nikita Koshelev <hukuta94@users.noreply.github.com>
2024-03-24 16:36:06 +0500

    fix code formatting

--- feature-sample/application/src/test/kotlin/com/hukuta94/beautysalon/featuresample/application/port/in/FeatureSampleUseCaseImplTest.kt
+++ feature-sample/application/src/test/kotlin/com/hukuta94/beautysalon/featuresample/application/port/in/FeatureSampleUseCaseImplTest.kt
@@ -6,7 +6,6 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class FeatureSampleUseCaseImplTest {
-
     @Test
     fun `usecase param is true - successful text message`() {
         FeatureSampleUseCaseImpl().execute(true).shouldBeRight()
